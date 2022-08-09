d0dbe7d5bad7b649fd16c8909f1beed7a9c6f803
Brien Thomas <thomasbrien94@gmail.com>
2022-08-09 10:35:42 -0700

    Stack addBack works within node, still working on test

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-class Stack {}
+class Stack {
+    #list;
+
+    constructor(initialList) {
+        if (initialList) this.#list = initialList;
+    }
+
+    addBack(item) {
+        this.#list.push(item);
+        return this.#list;
+    }
+
+}
+
+const dataStructure = new Stack([4, 3, 2])
+console.log(dataStructure.addBack(1));
 
 class Queue {}
 
--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
 const { Stack } = require("./index.js");
 describe("Stack Class", () => {
   it("#push should add a new item to the stack ", () => {
-    const stack = new Stack();
-    stack.push(1);
-    expect(stack.peek()).toEqual(1);
+    const stack = new Stack([4, 3, 2]);
+    stack.addBack(1);
+    expect(stack.peek()).toBe(1);
   });
   // add more tests here...
 });
